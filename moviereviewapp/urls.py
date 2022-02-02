97bf8e08a854d46a8f702943473b05048a5a1e80
Lennart <lenschepers@gmail.com>
2022-02-02 13:53:35 +0100

    added details page for individual movies

--- moviereviewapp/urls.py
+++ moviereviewapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('movies/', views.movies, name='movies'),
+    path('movies/<int:movie_id>/', views.detail, name='detail')
 ]
--- moviereviewapp/views.py
+++ moviereviewapp/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Movie
 
 def index(request):
@@ -9,4 +9,8 @@ def movies(request):
     movie_list = Movie.objects.all()
     context = {'movie_list': movie_list}
     return render(request, 'moviereviewapp/movies.html', context)
+
+def  detail(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    return render(request, 'moviereviewapp/detail.html', {'movie': movie})
 
