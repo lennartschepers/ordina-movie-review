de109adf98f9c674110d0e55074ad2feeacfc5b7
Lennart <lenschepers@gmail.com>
2022-02-11 10:55:15 +0100

    added image functionality for movie model

--- moviereviewapp/urls.py
+++ moviereviewapp/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 urlpatterns = [
@@ -6,6 +8,7 @@ urlpatterns = [
     path('movies/', views.movies, name='movies'),
     path('movies/<int:movie_id>/', views.detail, name='detail'),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('registration/register', views.register, name='register')
+    path('registration/register', views.register, name='register'),
 
 ]
+
