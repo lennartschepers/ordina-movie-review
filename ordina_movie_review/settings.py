16327c4ed0f7777727da66fc78bf42e2ea21ea21
Lennart <lenschepers@gmail.com>
2022-02-14 12:04:43 +0100

    added django_browser_reload to settings

--- ordina_movie_review/settings.py
+++ ordina_movie_review/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,7 +42,7 @@ INSTALLED_APPS = [
     # tailwind and theme are apps that provide tailwind functionality in django
     'tailwind',
     'theme',
-    'django-browser-reload',
+    'django_browser_reload',
 
     'moviereviewapp'
 ]
