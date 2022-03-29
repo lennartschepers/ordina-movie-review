379111f2523e29dfcfc028e3a2eaef4e033ffceb
Lennart <lenschepers@gmail.com>
2022-03-29 10:44:58 +0200

    removed rating from movie model, changed review model to 10 stars and changed rating to average of reviews

--- moviereviewapp/models.py
+++ moviereviewapp/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models import Avg
 
 
 # Create your models here.
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=1000)
-    rating = models.FloatField(null=True, blank=True, default=None)
     release_year = models.IntegerField(default=0)
     poster = models.ImageField()
 
@@ -16,6 +16,11 @@ class Movie(models.Model):
     def __str__(self):
         return f"{self.title} ({self.release_year})"
 
+    @property
+    def average_rating(self):
+        print(self.review_set.all().aggregate(Avg('rating')).get('rating__avg'))
+        return self.review_set.all().aggregate(Avg('rating')).get('rating__avg')
+
 
 class Review(models.Model):
     class Rating(models.IntegerChoices):
@@ -24,6 +29,11 @@ class Review(models.Model):
         THREE = 3
         FOUR = 4
         FIVE = 5
+        SIX = 6
+        SEVEN = 7
+        EIGHT = 8
+        NINE = 9
+        TEN = 10
 
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     body = models.TextField()
